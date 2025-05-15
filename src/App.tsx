ebe9fa77f59a7f704e8dc21a4527511a477eb4de
Gagik Antonyan <gagika57@gmail.com>
2025-05-15 20:58:33 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/styles.scss'
+
+const App: React.FC = () => {
+	return (
+		<div className='App'>
+			<h1>Barev, Ashxarh!</h1>
+		</div>
+	)
+}
+
+export default App
